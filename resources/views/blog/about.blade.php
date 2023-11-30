fa5808fdc15b988daf3cce0fb51a723c99cc95e9
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2023-11-30 15:03:17 +0100

    update .gitattributes and about.blade.php

--- resources/views/blog/about.blade.php
+++ resources/views/blog/about.blade.php
@@ -4,5 +4,5 @@
 
 @section('content')
     <h1>À propos</h1>
-    <p>Un inventaire des incroyables randonnées réalisées dans nos montagnes suisses !</p>
+    <p>Un inventaire des incroyables randonnées réalisées dans nos montagnes suisses.</p>
 @endsection
