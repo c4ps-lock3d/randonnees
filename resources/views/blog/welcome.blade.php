a3f73af132902878386c9065eaa97798aae31abc
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2024-02-25 11:39:07 +0100

    update welcome.blade.php

--- resources/views/blog/welcome.blade.php
+++ resources/views/blog/welcome.blade.php
@@ -28,13 +28,13 @@
                                 label: 'Nombre de randonnées',
                                 data: varChartCatAreaCount,
                                 backgroundColor: [
-                                    'rgba(255, 99, 132)',
-                                    'rgba(255, 159, 64)',
-                                    'rgba(255, 205, 86)',
-                                    'rgba(75, 192, 192)',
-                                    'rgba(54, 162, 235)',
-                                    'rgba(153, 102, 255)',
-                                    'rgba(201, 203, 207)'
+                                    'rgba(255, 99, 132, 0.5)',
+                                    'rgba(255, 159, 64, 0.5)',
+                                    'rgba(255, 205, 86, 0.5)',
+                                    'rgba(75, 192, 192, 0.5)',
+                                    'rgba(54, 162, 235, 0.5)',
+                                    'rgba(153, 102, 255, 0.5)',
+                                    'rgba(201, 203, 207, 0.5)'
                                 ],
                                 hoverOffset: 4
                             }],                  
