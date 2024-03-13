d4511dfd93f2b4e46b28aabef1579116630d5da5
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2024-03-13 23:25:49 +0100

    update show.blade.php

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -10,9 +10,9 @@
 <div class="row">
   <div class="col-lg-5 col-md-12 p-4">
   @if($postgpx->image)
-    <img src="../storage/{{ $postgpx->image }}" width="400" height="200" class="card-img-top" alt="...">
+    <img src="../storage/{{ $postgpx->image }}" width="400" class="card-img-top" alt="...">
   @else
-    <img src="{{url('img/9121424.jpg')}}" width="400" height="200" class="card-img-top" alt="...">
+    <img src="{{url('img/9121424.jpg')}}" width="400" class="card-img-top" alt="...">
   @endif
   </div>
   <div class="col-lg-7 col-md-12 pb-4">
